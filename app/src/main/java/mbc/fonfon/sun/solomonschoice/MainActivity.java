589c07fc1eb62e74c73343e8b1ce5da5b6481766
LeaderSung <iisungjuni@naver.com>
2016-10-05 16:57:15 +0900

    Initial commit

---
+++ app/src/main/java/mbc/fonfon/sun/solomonschoice/MainActivity.java
@@ -0,0 +1,13 @@
+package mbc.fonfon.sun.solomonschoice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
