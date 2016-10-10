25331d1e11d165d8a426bf94143f2a15dfc7054b
daraeKim <darae91@gmail.com>
2016-10-10 17:43:43 +0900

    김다래 수정 2016-

--- app/src/main/java/mbc/fonfon/sun/solomonschoice/MainActivity.java
+++ app/src/main/java/mbc/fonfon/sun/solomonschoice/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.SlidingDrawer;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -13,12 +15,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageView bestbtn;
     ImageView alrambtn;
     ImageView mypagebtn;
-    Button btnClose;
+    LinearLayout selectTitle;
+    FrameLayout content;
+    boolean viewGroupIsVisible = false;
+    TextView solmon;
+    LinearLayout selectmenu;
+    Button first, second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        first = (Button) findViewById(R.id.first);
+        second = (Button) findViewById(R.id.second);
+        solmon = (TextView) findViewById(R.id.solmon);
 
         homebtn = (ImageView) findViewById(R.id.homebtn);
         homebtn.setOnClickListener(this);
@@ -32,12 +43,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mypagebtn = (ImageView) findViewById(R.id.mypagebtn);
         mypagebtn.setOnClickListener(this);
 
-        btnClose = (Button)findViewById(R.id.btnclose);
-        btnClose.setOnClickListener(new View.OnClickListener() {
+        selectmenu = (LinearLayout) findViewById(R.id.selectmenu);
+
+        selectmenu.setVisibility(View.GONE);
+
+        content = (FrameLayout) findViewById(R.id.content);
+        content.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (viewGroupIsVisible) {
+                    selectmenu.setVisibility(View.GONE);
+                    viewGroupIsVisible = false;
+                }
+            }
+        });
+        selectTitle = (LinearLayout) findViewById(R.id.selectTitle);
+        selectTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SlidingDrawer drawer = (SlidingDrawer)findViewById(R.id.slide);
-                drawer.animateClose();
+                if (viewGroupIsVisible) {
+                    selectmenu.setVisibility(View.GONE);
+                    viewGroupIsVisible = false;
+                } else {
+                    selectmenu.setVisibility(View.VISIBLE);
+                    first.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            String text = (String) first.getText();
+                            first.setText(solmon.getText());
+                            solmon.setText(text);
+                            selectmenu.setVisibility(View.GONE);
+                            viewGroupIsVisible = !viewGroupIsVisible;
+                        }
+                    });
+
+                    second.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            String text = (String) second.getText();
+                            second.setText(solmon.getText());
+                            solmon.setText(text);
+                            selectmenu.setVisibility(View.GONE);
+                            viewGroupIsVisible = !viewGroupIsVisible;
+                        }
+                    });
+
+
+                    viewGroupIsVisible = true;
+
+                }
+
             }
         });
     }
@@ -48,28 +103,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.homebtn:
                 getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.frag_container_, new Photoboard())
+                        .replace(R.id.content, new Photoboard())
                         .commit();
                 break;
 
             case R.id.bestbtn:
                 getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.frag_container_, new Bestboard())
+                        .replace(R.id.content, new Bestboard())
                         .commit();
                 break;
 
             case R.id.alrambtn:
                 getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.frag_container_, new Alramboard())
+                        .replace(R.id.content, new Alramboard())
                         .commit();
                 break;
 
             case R.id.mypagebtn:
                 getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.frag_container_, new Mypage())
+                        .replace(R.id.content, new Mypage())
                         .commit();
                 break;
         }
